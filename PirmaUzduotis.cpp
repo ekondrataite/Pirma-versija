0209ae8e44604a969c96c0681d70b41313683515
ekondrataite <71488211+ekondrataite@users.noreply.github.com>
2020-09-27 14:27:13 +0300

    Add files via upload

--- PirmaUzduotis.cpp
+++ PirmaUzduotis.cpp
@@ -14,42 +14,65 @@
 using namespace std;
 
 struct studentas {
-    string vardas;
-    string pavarde;
-    float pazymiai[10];
-    float vidurkis;
-    float egzaminas;
-    float galutinis;
+	string vardas;
+	string pavarde;
+	int* pazymiai;
+	float egzaminas;
+	float galutinis;
 };
 
 int main()
 {
-    studentas eilute;
-    cout << "Iveskite studentu, kuriu duomenis pildysite, skaiciu: ";
-    int n = 0;
-    cin >> n;
-    studentas* grupe = new studentas[n];
+	cout << "Iveskite studentu, kuriu duomenis pildysite, skaiciu: ";
+	int n = 0;
+	cin >> n;
+	studentas* grupe = new studentas[n];
 
-    cout << "Iveskite studento varda, pavarde, egzamino ivertinima ir semestro namu darbu pazymius: \n";
-    for (int i = 0; i < n; i++) {
-        cout << i + 1 << "-ojo studento duomenys: "; 
-        cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egzaminas;
-    }
-    for (int i = 0; i < n; i++) {
-        cout << grupe[i].vardas << " : ";
-    }
-    cout << std::endl;
-    
-    studentas* temp = new studentas[n+1];
+	cout << "Iveskite studento varda, pavarde, egzamino ivertinima \n";
+	for (int i = 0; i < n; i++) {
+		cout << i + 1 << "-ojo studento duomenys: ";
+		cin >> grupe[i].vardas >> grupe[i].pavarde >> grupe[i].egzaminas;
 
-    for (int i = 0; i < n; i++) temp[i] = grupe[i];
+		cout << "Iveskite namu darbu pazymius: " << endl;
+		cout << "Spauskite Enter po kiekvieno ivesto pazymio. " << endl;
+		cout << "Norint baigti pazymiu vedima, iveskite 'end' ir spauskite Enter" << endl;
 
-    for (int i = 0; i < n; i++) {
-        cout << temp[i].vardas << " : ";
-    }
-    cout << std::endl;
+		int* temp = new int[100];
 
-    //cout << "Iveskite studento varda, pavarde, egzamino ivertinima ir semestro namu darbu pazymius: ";
-    //cin >> eilute.vardas >> eilute.pavarde >> eilute.egzaminas;
+		string dab;
 
+		int j = 0;
+		while (dab != "end") {
+			cin >> dab;
+			if (dab == "end") {
+				break;
+			}
+			temp[j] = std::stoi(dab);
+			j++;
+		} ;
+
+		grupe[i].pazymiai = new  int[j];
+
+		for (int m = 0; m < j; m++) {
+			grupe[i].pazymiai[m] = temp[m];
+		}
+
+		delete[] temp;
+	}
+
+	cout << "Ivesti duomenys: \n";
+	cout << "--------------------------\n";
+	for (int i = 0; i < n; i++) {
+		cout << grupe[i].vardas << " " << grupe[i].pavarde << " " << grupe[i].egzaminas;
+
+		int size = sizeof(grupe[i].pazymiai);
+		cout << "array size " << size << endl;
+		for (int k = 0; k < size - 1; k++) {
+			cout << " " << grupe[i].pazymiai[k];
+		}
+		cout << endl;
+		delete[] grupe[i].pazymiai;
+	}
+
+	delete[] grupe;
 }
